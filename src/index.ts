ec3dbe0557cd7e22d9741d48537775b6fda8c6f6
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-02-27 14:40:44 -0400

    add cors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { Server } from './config/server.js';
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { env } from './config/index.js';
+import cors, { type CorsOptions } from 'cors';
 
 (async () => {
     await Database.connect();
@@ -10,10 +12,32 @@ import morgan from 'morgan';
 })();
 
 async function main() {
+    // * server initialization
     const server = Server.init();
 
-    // * middlewares
+    // * proxy
+    server.set('trust proxy', 1);
+
+    //* cors configuration
+    const allowedOrigins: string[] = [env.baseUrl, 'http://localhost:5173', 'http://localhost:3000'];
+    const corsOptions: CorsOptions = {
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true);
+            if (env.isDev) return callback(null, true);
+            if (allowedOrigins.includes(origin)) return callback(null, true);
+
+            return callback(new Error('Not allowed by CORS'));
+        },
+
+        credentials: true,
+        optionsSuccessStatus: 200,
+    };
+
+    //* middlewares
     server.use(express.json());
     server.use(helmet());
     server.use(morgan('dev'));
+    server.use(cors(corsOptions));
+
+
 }
