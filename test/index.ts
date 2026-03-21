a1548eea2ccd4e3c55f6d6d192b7d2e89e912707
Wilfryn Viloria <138523300+iamwilllll@users.noreply.github.com>
2026-03-21 16:08:16 -0400

    Add test file

---
+++ test/index.ts
@@ -0,0 +1,46 @@
+import helmet from 'helmet';
+import express from 'express';
+import createCSM from '../dist/index.js';
+import mongoose from 'mongoose';
+import cors, { type CorsOptions } from 'cors';
+
+(async () => {
+    await mongoose.connect('mongodb+srv://wilfrynvil:em2U2AhRQGwRItMB@api.c4oqm5f.mongodb.net/apiDocs?appName=api');
+})();
+
+//* cors configuration
+const allowedOrigins: string[] = ['http://localhost:5173', 'http://localhost:3000'];
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) return callback(null, true);
+        return callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true,
+    optionsSuccessStatus: 200,
+};
+
+const app = express();
+
+app.use(cors(corsOptions));
+
+app.use('', createCSM({ mode: 'development', routePath: 'docs' }));
+
+app.get('/api', (req, res) => {
+    res.json({ message: 'Test route working!' });
+});
+
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: ["'self'"],
+                connectSrc: ["'self'", 'http://localhost:3000'],
+            },
+        },
+    })
+);
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
